ccd5c62e377efbda8a7f9322b8554efe3736e441
GreenTeodoro839 <77104800+GreenTeodoro839@users.noreply.github.com>
2026-09-09 11:14:33 +0800

    Modify LAN IP and DHCP address pool settings
    Update default LAN IP and DHCP settings in configuration files.

--- after-update-custom.sh
+++ after-update-custom.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 # Description: (After Update feeds)
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+# 默认 LAN IP
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
-# Set DHCP IP Address start and end
-sed -i "s/option start.*/option start \'2\'/g" package/network/services/dnsmasq/files/dhcp.conf
-sed -i "s/option limit.*/option limit \'55\'/g" package/network/services/dnsmasq/files/dhcp.conf
+# 主机名
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+
+# DHCP 地址池
+sed -i "s/option start.*/option start '100'/g" package/network/services/dnsmasq/files/dhcp.conf
+sed -i "s/option limit.*/option limit '150'/g" package/network/services/dnsmasq/files/dhcp.conf
